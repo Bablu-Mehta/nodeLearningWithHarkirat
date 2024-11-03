6aa5388bac64cbf7b7cdc64d2d4848dfe750e2fd
Bablu Mehta <babloomehta895012@gmail.com>
2024-11-03 21:13:59 +0530

    second assignment for fileServer is done and also added the route for 404

--- Week-2-Assignments-H/02-nodejs/fileServer.js
+++ Week-2-Assignments-H/02-nodejs/fileServer.js
@@ -26,7 +26,6 @@ const app = express();
 Response: 200 OK with an array of file names in JSON format.
 Example: GET http://localhost:3000/files
  */
-
 const dir = './files';
 
 app.get('/files', (req, res)=>{
@@ -37,6 +36,29 @@ app.get('/files', (req, res)=>{
     res.json(files);
   })
 })
+
+/**
+ * GET /file/:filename - Returns content of given file by name
+     Description: Use the filename from the request path parameter to read the file from `./files/` directory
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Example: GET http://localhost:3000/file/example.txt
+ */
+
+  app.get('/file/:filename', (req, res)=>{
+    const filePath = path.join(__dirname, './files/', req.params.filename);
+
+    fs.readFile(filePath,"utf-8", (err, data)=>{
+      if(err){
+        res.status(500).json({error: "Not able to read the file contents"})
+      }
+      res.status(200).send(data);
+    })
+
+  })
+
+  app.all('/*', (req, res) =>{
+    res.status(404).json({error: "Not able to find the path"});
+  })
 
 app.listen(3000, () => {
   console.log(`Example app listening on port`)
